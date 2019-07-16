421146681694422f5122c99569a43ff215815e63
James Hooper <jameshooperdesigns@gmail.com>
2019-07-15 22:50:37 -0600

    Add new changes to the UTA Motion app

--- app/home/home-item-detail/home-item-detail-page.js
+++ app/home/home-item-detail/home-item-detail-page.js
@@ -1,3 +1,54 @@
+// NEW
+const dialogsModule = require("tns-core-modules/ui/dialogs");
+const ogdenN = `Hello World!`;
+
+// New
+var observable = require("data/observable");
+var pageData = new observable.Observable();
+
+exports.loaded = function(args) {
+  pageData.set("NBtime", true);
+  args.object.bindingContext = pageData;  
+}
+
+exports.toggle = function() {
+pageData.set("NBtime", !pageData.get("NBtime"));
+};
+
+// End of NEW
+
+// This is for the Alert Function
+exports.alertOgdenN = function () {
+  dialogsModule.confirm({
+    title: "North Bound",
+    // message: "NO STOP   NO STOP   5:07 AM   5:37 AM  6:07 AM  6:37 AM   7:07 AM   7:37 AM  8:07 AM  8:37 AM   9:37 AM   10:37 AM   11:37 AM   12:37 PM   1:37 PM   NO STOP   2:37 PM   3:07 PM   3:37 PM   4:07 PM   4:37 PM   5:07 PM   5:37 PM   6:07 PM   6:37 PM   7:07 PM   8:07 PM   9:07 PM   10:07 PM   10:37 PM   11:07 PM",
+    message: ogdenN,
+    okButtonText: "CLOSE"
+  }).then(() => {
+    console.log("The user closed the alert.");
+  });
+};
+// This is for the Alert Function
+exports.alertOgdenS = function () {
+  // dialogsModule.alert({
+    dialogsModule.confirm({
+    title: "South Bound",
+    message: `6:00 AM  9:00 AM  10:00 AM  2:00 PM  3:30 PM 4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+    okButtonText: "CLOSE"
+  }).then(() => {
+    console.log("The user closed the alert.");
+  });
+};
+
+
+
+
+
+
+
+
+
+
 function onNavigatingTo(args) {
     const page = args.object;
 
@@ -13,4 +64,3 @@ function onBackButtonTap(args) {
 
 exports.onNavigatingTo = onNavigatingTo;
 exports.onBackButtonTap = onBackButtonTap;
-
--- app/home/home-items-view-model.js
+++ app/home/home-items-view-model.js
@@ -4,54 +4,160 @@ function HomeItemsViewModel() {
     const viewModel = observableModule.fromObject({
         items: [
             {
-                name: "Item 1",
-                description: "Description for Item 1"
+                name: "Ogden",
+                station: "Ogden Station",
+                address: "2393 South Wall Avenue",
+                city: "Ogden, UT 84511",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                routes: { 
+                    NBtime: "NO STOP   NO STOP   5:07 AM   5:37 AM  6:07 AM  6:37 AM   7:07 AM   7:37 AM  8:07 AM  8:37 AM   9:37 AM   10:37 AM   11:37 AM   12:37 PM   1:37 PM   NO STOP   2:37 PM   3:07 PM",
+                    SBtime: "9:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  "
+                },
+                // NBtimes: `NO STOP   NO STOP   5:07 AM   5:37 AM  6:07 AM  6:37 AM   7:07 AM   7:37 AM  8:07 AM  8:37 AM   9:37 AM   10:37 AM   11:37 AM   12:37 PM   1:37 PM   NO STOP   2:37 PM   3:07 PM   3:37 PM   4:07 PM   4:37 PM   5:07 PM   5:37 PM   6:07 PM   6:37 PM   7:07 PM   8:07 PM   9:07 PM   10:07 PM   10:37 PM   11:07 PM`,
+                // SBtimes: `9:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
             },
             {
-                name: "Item 2",
-                description: "Description for Item 2"
+                name: "Roy",
+                station: "Roy Station",
+                address: "4155 S. Sandridge Dr.",
+                city: "Roy, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `10:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 3",
-                description: "Description for Item 3"
+                name: "Clearfield",
+                station: "Clearfield Station",
+                address: "1250 S. State St.",
+                city: "Clearfield, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `11:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 4",
-                description: "Description for Item 4"
+                name: "Layton",
+                station: "Layton Station",
+                address: "150 S. Main St.",
+                city: "Layton, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `12:00 PM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 5",
-                description: "Description for Item 5"
+                name: "Farmington",
+                station: "Farmington Station",
+                address: "450 N. 850 W.",
+                city: "Farmington, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `1:00 PM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 6",
-                description: "Description for Item 6"
+                name: "Woods Cross",
+                station: "Wood Cross Station",
+                address: "750 S. 800 W.",
+                city: "Woods Cross, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `2:00 PM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 7",
-                description: "Description for Item 7"
+                name: "North Temple",
+                station: "North Temple Station",
+                address: "500 W. North Temple",
+                city: "Salt Lake City, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `3:00 PM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 8",
-                description: "Description for Item 8"
+                name: "Salt Lake Central",
+                station: "Salt Lake Central",
+                address: "250 S. 600 W.",
+                city: "Salt Lake City, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `4:00 PM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 9",
-                description: "Description for Item 9"
+                name: "Murray Central Station",
+                station: "Murray Central Station",
+                address: "140 W. Vine St.",
+                city: "Murray, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `5:00 PM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 10",
-                description: "Description for Item 10"
+                name: "South Jordan Station",
+                station: "South Jordan Station",
+                address: "10351 South Jordan Gateway",
+                city: "South Jordan, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `7:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 11",
-                description: "Description for Item 11"
+                name: "Draper Station",
+                station: "Draper Station",
+                address: "12997 S. FrontRunner Blvd.",
+                city: "Draper, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `6:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
-                name: "Item 12",
-                description: "Description for Item 12"
+                name: "Lehi Station",
+                station: "Lehi Station",
+                address: "3101 N. Ashton Blvd.",
+                city: "Lehi, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `5:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+            },
+            {
+                name: "American Fork Station",
+                station: "American Fork Station",
+                address: "782 W. 200 S.",
+                city: "American Fork, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `4:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+            },
+            {
+                name: "Orem Central",
+                station: "Orem Central Station",
+                address: "1350 W. 900 S.",
+                city: "Orem, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `3:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+            },
+            {
+                name: "Provo Station",
+                station: "Provo Station",
+                address: "690 S. University Ave.",
+                city: "Provo, UT",
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `2:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             }
-        ]
+        ],
     });
 
     return viewModel;
